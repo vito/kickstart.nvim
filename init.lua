7d4cb8524fa1e4829d0c380c5ee11b9813729f81
Alex Suraci <suraci.alex@gmail.com>
2024-05-18 13:06:39 -0400

    more ergonomic tab completion alternatives
    <C-n/p/y> feels like playing twister
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- init.lua
+++ init.lua
@@ -719,8 +719,10 @@ require('lazy').setup({
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
           ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-Tab>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-S-Tab>'] = cmp.mapping.select_next_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -730,6 +732,7 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-CR>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
