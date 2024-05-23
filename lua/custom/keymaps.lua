887773a56b476c851811b46e777ca03b4ba69f69
Alex Suraci <suraci.alex@gmail.com>
2024-05-23 10:21:59 -0400

    better j/k
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,6 +2,10 @@
 vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Escape with jk', noremap = true, silent = true })
 vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Escape with kj', noremap = true, silent = true })
 
+-- move by visual line, not actual line (i.e. wrapping)
+vim.keymap.set('n', 'j', 'gj', { desc = 'Move down visual line', noremap = true, silent = true })
+vim.keymap.set('n', 'k', 'gk', { desc = 'Move up visual line', noremap = true, silent = true })
+
 -- save on enter
 vim.keymap.set('n', '<CR>', function()
   if vim.api.nvim_buf_get_option(0, 'buftype') == 'quickfix' then
