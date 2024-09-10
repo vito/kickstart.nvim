7c4c49242e4bc816abb776ebcf496a7d6ccc6323
Alex Suraci <alex@dagger.io>
2024-09-10 13:39:12 -0400

    add templ (not used anymore but why not)
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- init.lua
+++ init.lua
@@ -596,6 +596,11 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        templ = {
+          cmd = { 'templ', 'lsp' },
+          filetypes = { 'templ' },
+          settings = {},
+        },
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
